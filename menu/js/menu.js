7eb3e6e78f2069d8c686802ba9c4ff41c4623de7
Clara <clara.antolin.garcia@gmail.com>
2018-01-12 20:31:27 +0100

    reestructuración de carpetas y archivos
    he añadido bootstrap y jquery que no sé si lo necesitaremos

---
+++ menu/js/menu.js
@@ -0,0 +1,9 @@
+<nav class="top-menu">
+<ul>
+    <li><a href="pages/place/place.html">¿Dónde nos casamos?</a></li>
+    <li><a href="pages/accommodation/accommodation.html">Alojamientos</a></li>
+    <li><a href="pages/transport/transport.html">Formas de llegar</a></li>
+    <li><a href="pages/weddingList/wedding-list.html">Lista de boda</a></li>
+    <li><a href="pages/foodMenu/food-menu.html">Menú</a></li>
+</ul>
+</nav>
