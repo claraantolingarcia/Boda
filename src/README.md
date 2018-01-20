5cb33ee9e8234aaab2ae2a57ace099a0e847aa3b
Daniel Fernandez <astro_titan@hotmail.com>
2018-01-20 15:10:29 +0100

    New files structure

---
+++ src/README.md
@@ -0,0 +1,26 @@
+# Boda
+Página web de nuestra boda
+
+
+class   -> .
+id      -> #
+
+
+ideas:
+
+hacer una pagina para que la gente pueda añadir posibles canciones para la boda
+
+menu:
+· Inicio
+· ¿donde nos casamos? -
+· Banquete
+· Formas de llegar
+· Lista de boda
+· Alojamiento
+· Canciones?
+· Itinerario?
+
+
+http://librosweb.es/libro/css/
+https://color.adobe.com/es/explore/?filter=most-popular&time=month
+https://getbootstrap.com/docs/3.3/css/#helper-classes
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,9 @@
 <nav class="top-menu">
 <ul>
-    <li><a href="pages/place/place.html">¿Dónde nos casamos?</a></li>
-    <li><a href="pages/accommodation/accommodation.html">Alojamientos</a></li>
-    <li><a href="pages/transport/transport.html">Formas de llegar</a></li>
-    <li><a href="pages/weddingList/wedding-list.html">Lista de boda</a></li>
-    <li><a href="pages/foodMenu/food-menu.html">Menú</a></li>
+    <li><a href="../html/place.html">¿Dónde nos casamos?</a></li>
+    <li><a href="../html/accommodation.html">Alojamientos</a></li>
+    <li><a href="../html/transport.html">Formas de llegar</a></li>
+    <li><a href="../html/wedding-list.html">Lista de boda</a></li>
+    <li><a href="../html/food-menu.html">Menú</a></li>
 </ul>
 </nav>
